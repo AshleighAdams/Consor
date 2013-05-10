2add1f20eb5e094c0c6875c6572084c532b00a67
Kate Adams <self@kateadams.eu>
2013-05-10 03:45:01 +0100

    Starting actual UI now

---
+++ Consor/Control.cpp
@@ -0,0 +1,32 @@
+#include "Control.hpp"
+
+using namespace Consor;
+
+CSize CControl::Size()
+{
+	return m_Size;
+}
+
+void CControl::OnResize(const CSize& Size)
+{
+}
+
+void CControl::ForceResize(const CSize& Size)
+{
+	m_Size = Size;
+	OnResize(Size);
+}
+
+void CControl::Draw(Consor::Console::IConsoleRenderer& Renderer, bool HasFocus, const Consor::ISkin& Skin)
+{
+}
+
+bool CControl::HandleInput(int Key)
+{
+	return false;
+}
+
+bool CControl::CanFocus()
+{
+	return false;
+}
