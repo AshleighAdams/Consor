905c850f1227d770ba869b5455b21a2b35e107cf
unknown <C0BRA@C0BRA-LT.(none)>
2013-05-10 15:39:53 +0100

    Fixed console renderer not using the correct offset (it was using the start of the renderbounds) Added CLable::ForceResize TODO: make CLable::ForceResize use elpisis on max height (impliment into WrapText

--- Consor/Controls/Lable.cpp
+++ Consor/Controls/Lable.cpp
@@ -1,4 +1,5 @@
 #include "Lable.hpp"
+#include "Util/StringUtils.hpp"
 
 using namespace Consor;
 
@@ -10,11 +11,23 @@ CLable::CLable()
 void CLable::SetText(std::string Text)
 {
 	m_Text = Text;
+	m_SetText = Text;
 	m_Size = CSize(Text.length(), 1);
+	OnResize(m_Size);
 }
 
 void CLable::Draw(Consor::Console::IConsoleRenderer& Renderer, bool HasFocus, const Consor::ISkin& Skin)
 {
 	CColour col = Skin.LableForeground();
 	Renderer.DrawString(m_Text, CVector(), Skin.LableForeground(), CColour::None());
+}
+
+void CLable::ForceResize(const CSize& Size)
+{
+	size_t width,height;
+
+	m_Text = Util::WrapText(m_SetText, Size.Width, &width, &height);
+	m_Size = CSize(width, (height > Size.Height ? height : Size.Height));
+
+	OnResize(m_Size);
 }
