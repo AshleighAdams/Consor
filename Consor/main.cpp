5e8082ee37ec6585aa455fe72b6bed1757a317e1
Kate Adams <self@kateadams.eu>
2013-05-10 03:54:31 +0100

    woop

--- Consor/main.cpp
+++ Consor/main.cpp
@@ -4,39 +4,32 @@
 #include "Util/StringUtils.hpp"
 #include "Util/Time.hpp"
 
+#include "Controls/Lable.hpp"
+
 using namespace std;
 
 int main(int count, char** values)
 {
 	Consor::Console::CWindowsConsoleRenderer renderer; 
 
-	Consor::CSize consolesize = renderer.RenderSize();;
-	renderer.PushRenderBounds(Consor::CVector(5, 5), consolesize - Consor::CSize(5, 5) * 2);
-	
-	Consor::CSize bounds = renderer.RenderSize();
+	Consor::CLable lbl;
+	lbl.SetText("Hello, world");
 
-	size_t height;
-	string input = "Lorem ipsum dolor sit amet, poop consectetur adipiscing elit. Pellentesque auctor tincidunt justo eu convallis. Integer fringilla condimentum dolor vitae tincidunt. Nunc vehicula elementum odio, condimentum scelerisque odio cursus id. Pellentesque ac quam nunc. Sed vitae iaculis dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis faucibus placerat augue iaculis cursus. Suspendisse id dolor auctor leo tristique tincidunt. Maecenas adipiscing imperdiet eros, ac pulvinar dui congue ut. Nulla facilisi. Nam ornare, felis et accumsan porttitor, dui velit luctus augue, nec dictum quam eros et risus. Phasellus convallis, nunc eget fringilla eleifend, nibh sapien posuere enim, ac ultrices mauris lorem in mauris. Sed a augue vel est convallis facilisis nec ut nisl.";
-	string trimmed = Consor::Util::WrapText(input, bounds.Width - 1, nullptr, &height);
+	Consor::CDefaultSkin skin;
 
 	while(true)
 	{
-		double start = Consor::Util::GetTime();
-
 		renderer.Clear(Consor::CColour());
 		
-		renderer.DrawRect(Consor::CVector(), Consor::CSize(bounds.Width, height + 1), Consor::CColour(1,1,1), Consor::CColour());
-		//renderer.DrawString(trimmed, Consor::CVector(1, 1), Consor::CColour(1, 1, 1), Consor::CColour(0, 0, 0, 0));
-
-		double end = Consor::Util::GetTime();
-		double span = end - start;
-
-		renderer.DrawString(Consor::Util::FormatString("FPS: %   ", 1.0/span), Consor::CVector(2,2), Consor::CColour(1, 1, 1), Consor::CColour(0, 0, 0, 0));
+		{
+			renderer.PushRenderBounds(Consor::CVector(5, 5), lbl.Size());
+			lbl.Draw(renderer, false, skin);
+			renderer.PopRenderBounds();
+		}
 
 		renderer.FlushToScreen();
 	}
 
-	renderer.PopRenderBounds();
 
 	return 0;
 }
