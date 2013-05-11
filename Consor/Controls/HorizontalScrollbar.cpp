96768af9c1e9a671bb1caa8568c8e77fe8cc4457
unknown <C0BRA@C0BRA-LT.(none)>
2013-05-11 19:36:50 +0100

    +Added detail to both ScrollBars by using the half-block chars

--- Consor/Controls/HorizontalScrollbar.cpp
+++ Consor/Controls/HorizontalScrollbar.cpp
@@ -40,5 +40,27 @@ void CHorizontalScrollbar::Draw(Consor::Console::IConsoleRenderer& Renderer, boo
 	CVector pos = CVector((int)((selfsize.Width - 1) * GetPercent()), 0);
 	CSize size = CSize(1, selfsize.Height);
 
-	Renderer.DrawBox(pos, size, fgcol);
+	if(fmod(pos.X, 1.0) > 0.5)
+	{
+		pos.X = (int)pos.X;
+
+		std::unique_ptr<Console::ICharInformation> infoleft = Renderer.GetCharInformation(pos);
+		std::unique_ptr<Console::ICharInformation> inforight = Renderer.GetCharInformation(pos + CVector(1, 0));
+
+		infoleft->SetChar((char)221);
+		inforight->SetChar((char)222);
+
+		infoleft->SetForegroundColour(fgcol);
+		inforight->SetForegroundColour(fgcol);
+	}
+	else
+	{
+		pos.X = (int)pos.X;
+		std::unique_ptr<Console::ICharInformation> info = Renderer.GetCharInformation(pos);
+
+		info->SetChar((char)219);
+		info->SetForegroundColour(fgcol);
+	}
+
+	//Renderer.DrawBox(pos, size, fgcol);
 }
