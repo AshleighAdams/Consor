a7a5e7b70cf72e850b57f146f3f267273aa5a3a1
Kate Adams <self@kateadams.eu>
2013-05-11 19:36:50 +0100

    +Added detail to both ScrollBars by using the half-block chars

--- Consor/Controls/VerticalScrollbar.cpp
+++ Consor/Controls/VerticalScrollbar.cpp
@@ -1,5 +1,6 @@
-#include "VerticalScrollbar.hpp"
+﻿#include "VerticalScrollbar.hpp"
 
+#include <string>
 using namespace Consor;
 
 CVerticalScrollbar::CVerticalScrollbar(const CSize& Size)
@@ -36,8 +37,29 @@ void CVerticalScrollbar::Draw(Consor::Console::IConsoleRenderer& Renderer, bool 
 
 	Renderer.DrawBox(CVector(), selfsize, bgcol);
 	
-	CVector pos = CVector(0, (int)((selfsize.Height - 1) * GetPercent()));
+	CVector pos = CVector(0, (selfsize.Height - 1) * GetPercent());
 	CSize size = CSize(selfsize.Width, 1);
 
-	Renderer.DrawBox(pos, size, fgcol);
+	if(fmod(pos.Y, 1.0) > 0.5)
+	{
+		pos.Y = (int)pos.Y;
+
+		std::unique_ptr<Console::ICharInformation> infotop = Renderer.GetCharInformation(pos);
+		std::unique_ptr<Console::ICharInformation> infobot = Renderer.GetCharInformation(pos + CVector(0, 1));
+
+		infotop->SetChar((char)220);
+		infobot->SetChar((char)223);
+
+		infotop->SetForegroundColour(fgcol);
+		infobot->SetForegroundColour(fgcol);
+	}
+	else
+	{
+		pos.Y = (int)pos.Y;
+		std::unique_ptr<Console::ICharInformation> info = Renderer.GetCharInformation(pos);
+
+		info->SetChar((char)219);
+		info->SetForegroundColour(fgcol);
+		//Renderer.DrawBox(pos, size, fgcol);
+	}
 }
