2bb943461167e84ab7ad146aaca85da52f184521
Kate Adams <self@kateadams.eu>
2013-05-14 02:01:23 +0100

    Added new WindowSystem Added Skin::Canvas

---
+++ Consor/WindowSystem.cpp
@@ -0,0 +1,127 @@
+#include "WindowSystem.hpp"
+#include "Util/Time.hpp"
+#include "InputSystem.hpp"
+
+using namespace Consor;
+using namespace std;
+using namespace WindowSystem::_priv_WindowSystem;
+
+bool WindowSystem::Setup(Console::IConsoleRenderer* Renderer, Input::IInputSystem* input)
+{
+	static bool ran = false;
+
+	if(ran) 
+		return false;
+	
+	m_pRenderer = Renderer;
+	m_pInput = input;
+	m_pSkin = std::shared_ptr<ISkin>(new CDefaultSkin(*Renderer));
+	m_Running = true;
+
+	// create the threads
+	m_DrawThread = thread([&]()
+	{
+		while(m_Running)
+		{
+			WindowSystem::Draw();
+			Util::Sleep(0.5);
+		}
+	});
+
+	m_InputThread = thread([&]()
+	{
+		while(m_Running)
+		{
+			Input::Key k = m_pInput->GetKeyPress();
+			HandleInput(k, *m_pInput);
+			Draw();
+		}
+	});
+
+	return true;
+}
+
+void WindowSystem::_priv_WindowSystem::m_SetSkin(std::shared_ptr<ISkin> skin)
+{
+	m_pSkin = skin;
+}
+
+void WindowSystem::Draw()
+{
+	m_Mutex.lock();
+
+	Console::IConsoleRenderer& renderer = Renderer();
+
+	renderer.Clear(m_pSkin->Canvas());
+	
+	for(windowinfo_t& info : m_Registered)
+	{
+		Consor::CSize size = info.pControl->Size();
+		Consor::CSize rsize = renderer.RenderSize();
+		Consor::CVector pos = info.position;
+
+		if(info.position.X < 0)
+			pos.X = rsize.Width / 2.0 - size.Width / 2;
+		
+		if(info.position.Y < 0)
+			pos.Y = rsize.Height / 2.0 - size.Height / 2;
+
+		renderer.PushRenderBounds(pos, size);
+		info.pControl->Draw(renderer, &info == &m_Registered.back(), *m_pSkin);
+		renderer.PopRenderBounds();
+	}
+
+	renderer.FlushToScreen();
+
+	m_Mutex.unlock();
+}
+
+void WindowSystem::HandleInput(Input::Key key, Input::IInputSystem& is)
+{
+	m_Mutex.lock();
+
+	if(m_Registered.begin() == m_Registered.end())
+	{
+		m_Mutex.unlock();
+		return;
+	}
+	m_Registered.back().pControl->HandleInput(key, is);
+
+	m_Mutex.unlock();
+}
+
+ void WindowSystem::RegisterWindow(CControl& control, CVector pos)
+ {
+	windowinfo_t info;
+	info.position = pos;
+	info.pControl = &control;
+
+	m_Mutex.lock();
+	m_Registered.push_back(info);
+	m_Mutex.unlock();
+ }
+
+void WindowSystem::UnregisterWindow(CControl& control)
+{
+	m_Mutex.lock();
+	m_Registered.remove_if([&](const windowinfo_t& info)
+	{
+		return info.pControl == &control;
+	});
+	m_Mutex.unlock();
+}
+
+Console::IConsoleRenderer& WindowSystem::Renderer()
+{
+	return *m_pRenderer;
+}
+
+void WindowSystem::Lock()
+{
+	m_Mutex.lock();
+}
+
+void WindowSystem::Unlock()
+{
+	m_Mutex.unlock();
+}
---
+++ Consor/WindowSystem.hpp
@@ -0,0 +1,60 @@
+#ifndef CONSOR_WINDOWSYSTEM_H
+#define CONSOR_WINDOWSYSTEM_H
+
+#include "ConsoleRenderer.hpp"
+#include "Control.hpp"
+#include <mutex>
+#include <memory>
+#include <thread>
+
+namespace Consor
+{
+	struct windowinfo_t
+	{
+		CControl* pControl;
+		CVector position;
+	};
+
+	
+	namespace WindowSystem
+	{
+		namespace _priv_WindowSystem
+		{
+			static std::mutex m_Mutex;
+			static Console::IConsoleRenderer* m_pRenderer;
+			static Input::IInputSystem* m_pInput;
+			static std::shared_ptr<ISkin> m_pSkin;
+			static std::thread m_InputThread;
+			static std::thread m_DrawThread;
+			static std::list<windowinfo_t> m_Registered;
+			static bool m_Running;
+
+			extern void m_SetSkin(std::shared_ptr<ISkin> skin);
+		};
+
+		extern bool Setup(Console::IConsoleRenderer* Renderer, Input::IInputSystem* input);
+
+		extern Console::IConsoleRenderer& Renderer();
+		extern void Draw();
+		extern void Lock();
+		extern void Unlock();
+		extern void HandleInput(Input::Key key, Input::IInputSystem& is);
+		extern void RegisterWindow(CControl& control, CVector pos);
+		extern void UnregisterWindow(CControl& control);
+
+		template <class Skin>
+		static std::shared_ptr<ISkin> SetSkin()
+		{
+			using namespace _priv_WindowSystem;
+			m_Mutex.lock();
+			std::shared_ptr<ISkin> skin = std::shared_ptr<ISkin>(new Skin(Renderer()));
+			m_SetSkin(skin);
+			m_Mutex.unlock();
+			return skin;
+		}
+
+		
+	};
+};
+
+#endif
