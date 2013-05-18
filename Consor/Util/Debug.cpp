5756f2a863db7ee24ad2f5c76e6142beb4b0f7ed
c0bra61 <c0bra@xiatek.org>
2013-05-18 05:08:57 +0100

    Log hooking

--- Consor/Util/Debug.cpp
+++ Consor/Util/Debug.cpp
@@ -2,14 +2,27 @@
 #include "Time.hpp"
 
 #include <iostream>
+#include <list>
 
 using namespace std;
+using namespace Consor;
+using namespace Consor::Util;
 
-void Consor::Util::Log(const string& Message)
+std::list<LogCallback> _Subscribed;
+
+void Util::Log(const string& Message)
 {
 	double time = Util::GetTime();
 	time = ((double)(int)(time * 1000) / 1000.0);
 	string fin = Util::FormatString("[%] %", time, Message);
 
 	cerr << fin << "\n";
+
+	for(LogCallback cb : _Subscribed)
+		cb(fin);
+}
+
+void Util::HookLog(LogCallback callback)
+{
+	_Subscribed.push_back(callback);
 }
