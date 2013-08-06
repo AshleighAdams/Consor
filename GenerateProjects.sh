fd2011f6cc5f062f979e4f773edfd0eb5e8bedf6
Kobra <kobra@laptop>
2013-08-06 14:17:42 +0100

    Travis

--- GenerateProjects.sh
+++ GenerateProjects.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
 
-
-
-
-PS3='Please enter your choice: '
-options=("GNU Make" "Code::Blocks")
-toolset="gmake" 
-
-select opt in "${options[@]}"
-do
-    case $opt in
-        "GNU Make")
-            toolset="gmake"
-            break
-            ;;
-        "Code::Blocks")
-            toolset="codeblocks"
-            break
-            ;;
-    esac
-done
-
-# clean existing projects
 ThirdParty/Premake4.elf --file=Consor.lua clean
-ThirdParty/Premake4.elf --file=Consor.lua $toolset
+ThirdParty/Premake4.elf --file=Consor.lua gmake
