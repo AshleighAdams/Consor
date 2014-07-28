8b5195293ad6e8c3762c0864c4120cf6c27d15e5
Kate Adams <self@kateadams.eu>
2014-07-28 02:45:08 +0100

    Doxygen documentation.

--- Source/ConsoleRenderer.hpp
+++ Source/ConsoleRenderer.hpp
@@ -27,7 +27,7 @@ namespace Consor
 
 			virtual void SetChar(char val) = 0;
 			virtual char GetChar() = 0;
-
+			
 			virtual bool SupportsUnicode() = 0;
 			virtual void SetUnicodeChar(char32_t val) = 0;
 			virtual char32_t GetUnicodeChar() = 0;
@@ -57,38 +57,66 @@ namespace Consor
 			virtual ~IConsoleRenderer(){}
 
 			// platform specific stuff
+			/// \return The renderer's name.
 			virtual std::string RendererName();
+			/// \return Information about the renderer.  Such as: `"built Jul 18 2014 23:00:36; abstract renderer; impliments and supports: 256 custom colours, unicode; direct draw calls: none; abstract draw calls: DrawBox, DrawRect, DrawString; flushmode: all; built with: GNU GCC/G++ 4.9.0; C++ version: 201103; bits: 64;"`
 			virtual std::string VersionString() = 0;
 
+			/// Write the UI to the console.
 			virtual void FlushToScreen() = 0;
+			/// Get information about a specific char at the specified position.
 			virtual std::unique_ptr<ICharInformation> GetCharInformation(const Vector& pos) = 0;
+			/// \return Size of the console.
 			virtual Size GetSize() = 0;
 
+			/// \return If the console supports Unicode, `true`; otherwise `false`.
 			virtual bool SupportsUnicode() = 0;
-			// this will probably replace existing colours
+			/// \return Number of colours supported by the console.
 			virtual size_t MaxColours() = 0;
+			/// Fill the array `pColours` with the current console colours.
+			/// \param Count The size of our array to prevent buffer overflows, should be equal to `this->MaxColours()`.
 			virtual void GetColours(size_t Count, Colour* pColours) = 0;
+			/// Set the console colours from the array `pColours`.
+			/// \param Count The size of our array to prevent buffer overflows, should be equal to `this->MaxColours()`.
+			/// \warn If you wish to create a colour, you should use `RequestColour`!
 			virtual void SetColours(size_t Count, Colour* pColours) = 0;
+			/// Reset the colours to the console's defaults.
 			virtual void ResetColours() = 0;
 			// this function is a helper which uses the ones above, if you directly modify the colour table
 			// don't be supprised if bugs arise
+			/// Get the closest colour possible.  Will create it if `make == true`.
 			virtual Colour RequestColour(const Colour& target, bool make = false);
+			/// When colours have been modified, this should be called.
+			/// \warn It is the console renderer's job to call this.
 			virtual void FlushRequestedColours();
 
+			/// Set the console's title.
+			/// \param Title The string to set the title to.
 			virtual void SetTitle(const std::string& Title) = 0;
 
 			// other stuff
+			/// Clears the console.
+			/// \param col The colour used to clear.
 			virtual void Clear(const Colour& col);
-
+			
+			/// Draws a filled box.
 			virtual void DrawBox(const Vector& pos, const Size& size, const Colour& col);
+			/// Draws a rectangle with the specified foreground and background colours.
 			virtual void DrawRect(const Vector& pos, const Size& size, const Colour& fg_col, const Colour& bg_col);
 			virtual void DrawString(const std::string& str, const Vector& pos, const Colour& fgcol, const Colour& bgcol);
 			
+			/// Draw functions will see 0, 0 at the position `from`, and can draw to `size`.  Any drawing outside of that range is clipped.
 			virtual void PushRenderBounds(const Vector& from, const Consor::Size& size);
+			/// Pop a render bound off of the stack.
 			virtual void PopRenderBounds();
+			/// The available space to render our content in.
 			virtual Size RenderSize();
+			/// \return The absolute position of the current render bounds.
 			virtual Vector RenderOffset();
+			/// Is the position inside of the render bounds?
+			/// \param dir The direction it is out of bounds in.
 			virtual bool InRenderBounds(const Vector& pos, Vector* dir);
+			/// Is the box inside of the render bounds?
 			virtual bool InRenderBounds(const Vector& pos, const Consor::Size& size);
 		};
 	}
--- Source/Util/Math.hpp
+++ Source/Util/Math.hpp
@@ -7,6 +7,9 @@ namespace Consor
 {
 	namespace Util
 	{
+		/// Round the value to the specified percision.
+		/// \param Value The number to round.
+		/// \param Precision The smallest unit possible.  Example: 0.25, then 1.3 is rounded to 1.25, 1.9 is rounded to 2.0.
 		template<typename T> 
 		T Round(T Value, T Precision)
 		{
@@ -14,8 +17,17 @@ namespace Consor
 			return floor(Value * Precision + T(0.5)) / Precision;
 		}
 
+		/// Scale or map a number from one range to another.
+		/// \param Value The input value.
+		/// \param FromMin The minimum input value.
+		/// \param FromMax The maximum input value.
+		/// \param ToMin The input minimum will map to this.
+		/// \param ToMax The input maximum will map to this.
+		/// \return `Value` mapped to the from-range to the to-range.
+		/// \warning The input value can go out of bounds, and will be extrapolated.
+		/// \warning The range minimium does not have to be less than the range maximium.
 		template<typename T>
-		T Scale(T Value, T FromMin, T FromMax, T ToMax, T ToMin)
+		T Scale(T Value, T FromMin, T FromMax, T ToMin, T ToMax)
 		{
 			T from_diff = FromMax - FromMin;
 			T to_diff = ToMax - ToMin;
