64c1a98de4abe5f91e4a92c98ea047e9d7ccfa66
Kate Adams <self@kateadams.eu>
2014-07-28 08:10:32 +0100

    Control documentation.

--- Source/Control.hpp
+++ Source/Control.hpp
@@ -10,17 +10,28 @@
 
 namespace Consor
 {
+	/// The base control.
 	class Control
 	{
 	protected:
 		Consor::Size _Size;
 	public:
 		virtual ~Control(){}
+		/// Returns/calculates the size of the control.
 		virtual Size GetSize();
+		/// Called when the size of the control is changed.
 		virtual void OnResize(const Size& Size);
+		/// Attempts to force the control to a certain size constraint.
 		virtual void ForceResize(const Size& Size);
+		/// Draw itself at 0, 0, to `GetSize()`
+		/// \param Renderer The renderer to draw to.
+		/// \param HasFocus Whether or not the control is currently focused.
+		/// \param Skin Used to select suitble colours to draw with.
 		virtual void Draw(Consor::Console::IConsoleRenderer& Renderer, bool HasFocus, const Consor::ISkin& Skin);
+		/// Called when the control is focused and there is input.
+		/// \return `true` if we done something with the event to prevent parent controls from handeling the input (such as `ScrollContainer`s scrolling in a `TextBox`).
 		virtual bool HandleInput(Input::Key Key, Input::IInputSystem& System);
+		/// \return If the control is able to receive focus/or input.
 		virtual bool CanFocus();
 	};
 };
