c54f605b793aaa80a1fc06542e7a46639a2c64f5
Kate Adams <self@kateadams.eu>
2014-07-28 08:10:39 +0100

    Window system documentation.

--- Source/WindowSystem.hpp
+++ Source/WindowSystem.hpp
@@ -23,18 +23,33 @@ namespace Consor
 		{
 			extern void _SetSkin(std::shared_ptr<ISkin> skin);
 		};
-
+		
+		/// Setup the window system with the passed console renderer and input system.
 		extern bool Setup(Console::IConsoleRenderer* Renderer, Input::IInputSystem* input);
 
+		/// \return A reference to the renderer.
 		extern Console::IConsoleRenderer& Renderer();
+		/// Tell the renderer to draw.
 		extern void Draw();
+		/// For thready safty when interfacing from other threads.
 		extern void Lock();
+		/// For thready safty when interfacing from other threads.
 		extern void Unlock();
 		extern void HandleInput(Input::Key key, Input::IInputSystem& is);
+		/// Register and place a new control at the top of the stack.
+		/// \param control The control to be registered.
+		/// \param The position to place it.  `-1` means center, a value between `0` and `1` is a percentage, and values `>= 1` are absolute.
 		extern void RegisterWindow(Control& control, Vector pos);
+		/// Remove a control from the stack.
 		extern void UnregisterWindow(Control& control);
 
 		// automatically unregistered when pControl is unregistered (if not nullptr)
+		/// Registers a hotkey, and calls `callback` when it is pushed.
+		/// \param pControl The control the hotkey belongs to; should idealy be a window.  If this is not `nullptr`, then when the control is unregistered via `UnregisterWindow()`, the hotkey is removed.
+		/// \param Key The key needed to activate the hotkey.
+		/// \param Control Whether the control modifier needs to be pressed.
+		/// \param Shift Whether the shift modifier needs to be pressed.
+		/// \param callback The function to call upon the hotkey being activated.
 		void RegisterHotKey(Control* pControl, Input::Key Key, bool Control, bool Shift, std::function<void()> callback);
 
 		extern bool Running();
