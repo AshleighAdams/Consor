d05f2e9ee6bce4e633150adeb4903d7db54af69a
Kate Adams <self@kateadams.eu>
2014-07-28 08:31:17 +0100

    PasswordBox documentation.

--- Source/Controls/PasswordBox.hpp
+++ Source/Controls/PasswordBox.hpp
@@ -13,8 +13,11 @@ namespace Consor
 		char _PasswordChar;
 	public:
 		PasswordBox();
+		/// `TextBox` uses `GetText()` to render the text.  This re-impliments that function to censor the password.
 		virtual std::string GetText();
+		/// Get the entered string, uncensored.
 		virtual std::string GetPassword();
+		/// Set the char used to censor the password.
 		virtual void SetPasswordChar(char Letter);
 	};
 }
