02bf21706e4c10b1746dabf8a3c09ce8b8a980dc
Kate Adams <self@kateadams.eu>
2014-08-06 07:35:25 +0100

    Added WindowContainer::Show|Close methods.  They also pause the current thread until the window is closed using mutexes.

--- Source/Containers/WindowContainer.hpp
+++ Source/Containers/WindowContainer.hpp
@@ -2,7 +2,10 @@
 #define CONSOR_WINDOWCONTAINER_H
 
 #include "../Control.hpp"
+#include "../Units.hpp"
+
 #include <string>
+#include <mutex>
 
 namespace Consor
 {
@@ -12,8 +15,16 @@ namespace Consor
 	protected:
 		Control& _Client;
 		std::string _Title;
+		/// Unlock this to allow Show() to return!
+		std::mutex _RunningMutex;
+		bool _Running;
 	public:
 		WindowContainer(Control& Client, const std::string& Title);
+		~WindowContainer();
+		/// Show the window (using WindowSystem), Position is the same as on RegisterWindow()
+		void Show(const Vector& Position = Vector(-1, -1));
+		void Close();
+		
 		virtual Size GetSize();
 		virtual void OnResize(const Size& Size);
 		virtual void ForceResize(const Size& Size);
